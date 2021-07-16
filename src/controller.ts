c6a2fbe33f15d825b1dc08eaea7263afd1d9fcb0
Florian Morel <ayamflow@gmail.com>
2021-07-16 21:09:11 +0200

    add file input feature

--- src/controller.ts
+++ src/controller.ts
@@ -1,61 +1,43 @@
-import {
-	constrainRange,
-	Controller,
-	PointerHandler,
-	PointerHandlerEvent,
-	Value,
-	ViewProps,
-} from '@tweakpane/core';
+import {Controller, Value, ViewProps} from '@tweakpane/core';
 
 import {PluginView} from './view';
 
 interface Config {
-	value: Value<number>;
+	value: Value<HTMLImageElement>;
+	extensions: string[];
 	viewProps: ViewProps;
 }
 
-// Custom controller class should implement `Controller` interface
 export class PluginController implements Controller<PluginView> {
-	public readonly value: Value<number>;
+	public readonly value: Value<HTMLImageElement>;
 	public readonly view: PluginView;
 	public readonly viewProps: ViewProps;
 
 	constructor(doc: Document, config: Config) {
-		this.onPoint_ = this.onPoint_.bind(this);
-
-		// Receive the bound value from the plugin
 		this.value = config.value;
-
-		// and also view props
 		this.viewProps = config.viewProps;
-		this.viewProps.handleDispose(() => {
-			// Called when the controller is disposing
-			console.log('TODO: dispose controller');
-		});
 
-		// Create a custom view
 		this.view = new PluginView(doc, {
 			value: this.value,
 			viewProps: this.viewProps,
+			extensions: config.extensions,
 		});
 
-		// You can use `PointerHandler` to handle pointer events in the same way as Tweakpane do
-		const ptHandler = new PointerHandler(this.view.element);
-		ptHandler.emitter.on('down', this.onPoint_);
-		ptHandler.emitter.on('move', this.onPoint_);
-		ptHandler.emitter.on('up', this.onPoint_);
+		this.onFile_ = this.onFile_.bind(this);
+		this.view.input.addEventListener('change', this.onFile_);
+
+		this.viewProps.handleDispose(() => {
+			this.view.input.removeEventListener('change', this.onFile_);
+		});
 	}
 
-	private onPoint_(ev: PointerHandlerEvent) {
-		const data = ev.data;
-		if (!data.point) {
-			return;
-		}
+	private onFile_(event: Event): void {
+		const files = (event?.target as HTMLInputElement).files;
+		if (!files || !files.length) return;
 
-		// Update the value by user input
-		const dx =
-			constrainRange(data.point.x / data.bounds.width + 0.05, 0, 1) * 10;
-		const dy = data.point.y / 10;
-		this.value.rawValue = Math.floor(dy) * 10 + dx;
+		const file = files[0];
+		const image = document.createElement('img');
+		image.src = URL.createObjectURL(file);
+		this.value.rawValue = image;
 	}
 }
