622ce2d209cab4ec618b405cb83a840ff4df0948
Florian Morel <ayamflow@gmail.com>
2021-07-16 21:09:11 +0200

    add file input feature

--- src/view.ts
+++ src/view.ts
@@ -1,77 +1,48 @@
-import {ClassName, mapRange, Value, View, ViewProps} from '@tweakpane/core';
+import {ClassName, Value, View, ViewProps} from '@tweakpane/core';
 
 interface Config {
-	value: Value<number>;
+	value: Value<HTMLImageElement>;
 	viewProps: ViewProps;
+	extensions: string[];
 }
 
-// Create a class name generator from the view name
-// ClassName('tmp') will generate a CSS class name like `tp-tmpv`
+const DEFAULT_EXTENSIONS = ['.jpg', '.png', '.gif'];
+
 const className = ClassName('img');
 
-// Custom view class should implement `View` interface
 export class PluginView implements View {
 	public readonly element: HTMLElement;
-	private value_: Value<number>;
-	private dotElems_: HTMLElement[] = [];
-	private textElem_: HTMLElement;
+	public readonly input: HTMLElement;
+	private value_: Value<HTMLImageElement>;
+	private image_: HTMLImageElement;
 
 	constructor(doc: Document, config: Config) {
-		// Create a root element for the plugin
 		this.element = doc.createElement('div');
 		this.element.classList.add(className());
-		// Bind view props to the element
 		config.viewProps.bindClassModifiers(this.element);
 
-		// Receive the bound value from the controller
 		this.value_ = config.value;
-		// Handle 'change' event of the value
 		this.value_.emitter.on('change', this.onValueChange_.bind(this));
 
-		// Create child elements
-		this.textElem_ = doc.createElement('div');
-		this.textElem_.classList.add(className('text'));
-		this.element.appendChild(this.textElem_);
+		this.input = doc.createElement('input');
+		this.input.classList.add(className('input'));
+		this.input.setAttribute('type', 'file');
+		this.input.setAttribute(
+			'accept',
+			(config.extensions ?? DEFAULT_EXTENSIONS).join(','),
+		);
+		this.element.appendChild(this.input);
 
-		// Apply the initial value
+		this.image_ = doc.createElement('img');
+		this.image_.classList.add(className('image'));
+		this.element.appendChild(this.image_);
+
 		this.refresh_();
-
-		config.viewProps.handleDispose(() => {
-			// Called when the view is disposing
-			console.log('TODO: dispose view');
-		});
 	}
 
 	private refresh_(): void {
 		const rawValue = this.value_.rawValue;
-
-		this.textElem_.textContent = rawValue.toFixed(2);
-
-		while (this.dotElems_.length > 0) {
-			const elem = this.dotElems_.shift();
-			if (elem) {
-				this.element.removeChild(elem);
-			}
-		}
-
-		const doc = this.element.ownerDocument;
-		const dotCount = Math.floor(rawValue);
-		for (let i = 0; i < dotCount; i++) {
-			const dotElem = doc.createElement('div');
-			dotElem.classList.add(className('dot'));
-
-			if (i === dotCount - 1) {
-				const fracElem = doc.createElement('div');
-				fracElem.classList.add(className('frac'));
-				const frac = rawValue - Math.floor(rawValue);
-				fracElem.style.width = `${frac * 100}%`;
-				fracElem.style.opacity = String(mapRange(frac, 0, 1, 1, 0.2));
-				dotElem.appendChild(fracElem);
-			}
-
-			this.dotElems_.push(dotElem);
-			this.element.appendChild(dotElem);
-		}
+		this.image_.setAttribute('src', rawValue.src);
 	}
 
 	private onValueChange_() {
